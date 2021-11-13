f9e44ece486e4bd297cee8129a19d90905dd58a6
Gingka/Ginger Pepper <gingkathfox@gmail.com>
2021-11-12 23:29:25 -0500

    And with that, we are done!

--- src/endpoints/inflation.js
+++ src/endpoints/inflation.js
@@ -1,3 +1,21 @@
 const {inputValidation, request} = require('../util/util')
 module.exports = {
+    /**
+     * Fetch inflation information.
+     * @returns {Object}
+     */
+    getInflation() {
+        return request({
+            subUrl: '/inflation'
+        })
+    },
+    /**
+     * Fetch Solana supply info provided by the RPC.
+     * @returns {Object}
+     */
+    getSupply() {
+        return request({
+            subUrl: '/supply'
+        })
+    }
 }
